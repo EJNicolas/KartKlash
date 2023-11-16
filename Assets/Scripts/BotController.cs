42232ec96823c36cf203bded0631da0aae1d802b
Kybre <hermanchan10@gmail.com>
2023-11-15 22:33:52 -0800

    Revert "Merge branch 'ai-driving'"
    This reverts commit 78420b84bb499692de043291fd0bfba58dbff806, reversing
    changes made to 360097b97aa2cd266933b7f168af4bbd5ab11c44.

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -8,41 +8,14 @@ public class BotController : Entity
     [Header("Navigation")]
     public GameObject[] listPos;
 
+    //public GameObject botObj;
     public NavMeshAgent nma;
-    public RaceManager rm;
     public float rdThreshold;
-    public int currentPoint, checkpointsPassed;
-    public int rubberbandThreshold;
-
-    enum RubberbandState
-    {
-        SLOW,
-        NORMAL,
-        FAST
-    }
-    RubberbandState rs = RubberbandState.NORMAL;
-    bool stateChanged = false;
-
-    //navmeshagent fields
-    private float defaultSpeed, defaultAngular, defaultAccel;
-    public float speedRubberbandAmount, angularRubberbandFactor, accelRubberbandAmount;
-
-    //public GameObject trackedObj; //player
+    int currentPoint;
 
     void Start()
     {
-        currentPoint = listPos.Length;
-
-        //nma fields
-        defaultSpeed = nma.speed;
-        defaultAngular = nma.angularSpeed;
-        defaultAccel = nma.acceleration;
-
-        if (speedRubberbandAmount == 0) speedRubberbandAmount = 10;
-        if (angularRubberbandFactor == 0) angularRubberbandFactor = 2;
-        if (accelRubberbandAmount == 0) accelRubberbandAmount = 2;
-
-        rm = FindAnyObjectByType<RaceManager>();
+        currentPoint = 0;
     }
 
     // Update is called once per frame
@@ -56,62 +29,11 @@ public class BotController : Entity
         //navigation
         nma.enabled = true;
         if (nma.hasPath && nma.remainingDistance < nma.stoppingDistance + rdThreshold && currentPoint < listPos.Length)
-        {
             currentPoint++;
-            checkpointsPassed++;
-            //Debug.Log(this.gameObject.name + ", Checkpoints: " + checkpointsPassed);
-        }
-        else if (currentPoint >= listPos.Length) currentPoint = 0;
         if (currentPoint < listPos.Length)
             nma.SetDestination(new Vector3(listPos[currentPoint].transform.position.x,
                 this.gameObject.transform.position.y,
                 listPos[currentPoint].transform.position.z));
-
-        if (Mathf.Abs(checkpointsPassed - rm.checkpointsPassed) > rubberbandThreshold || 
-            rs == RubberbandState.SLOW && Mathf.Abs(checkpointsPassed - rm.checkpointsPassed) < rubberbandThreshold ||
-            rs == RubberbandState.FAST && Mathf.Abs(checkpointsPassed - rm.checkpointsPassed) < rubberbandThreshold) 
-            stateChanged = false;
-
-        if(!stateChanged) NavRubberband();
-    }
-
-    void NavRubberband()
-    {
-        if (checkpointsPassed - rm.checkpointsPassed < -rubberbandThreshold) rs = RubberbandState.FAST;
-        else if (checkpointsPassed - rm.checkpointsPassed > rubberbandThreshold) rs = RubberbandState.SLOW;
-        else rs = RubberbandState.NORMAL;
-
-        Debug.Log(this.gameObject.name + ", rb value:" + (checkpointsPassed - rm.checkpointsPassed));
-
-        stateChanged = true;
-
-        switch (rs)
-        {
-            case RubberbandState.FAST:
-                Debug.Log(this.gameObject.name + ", Speeding Up");
-                nma.speed = defaultSpeed + (speedRubberbandAmount * Mathf.Abs(checkpointsPassed - rm.checkpointsPassed));
-                nma.angularSpeed = defaultAngular * angularRubberbandFactor;
-                nma.acceleration = defaultAccel + (accelRubberbandAmount * Mathf.Abs(checkpointsPassed - rm.checkpointsPassed));
-                return;
-
-            case RubberbandState.NORMAL:
-                Debug.Log(this.gameObject.name + ", Returning to Normal");
-                nma.speed = defaultSpeed;
-                nma.angularSpeed = defaultAngular;
-                nma.acceleration = defaultAccel;
-                return;
-
-            case RubberbandState.SLOW:
-                Debug.Log(this.gameObject.name + ", Slowing Down");
-                nma.speed = defaultSpeed - (speedRubberbandAmount * Mathf.Abs(checkpointsPassed - rm.checkpointsPassed));
-                return;
-
-            default:
-                nma.speed = defaultSpeed;
-                nma.angularSpeed = defaultAngular;
-                nma.acceleration = defaultAccel;
-                return;
-        }
     }
 
     void Shoot()
