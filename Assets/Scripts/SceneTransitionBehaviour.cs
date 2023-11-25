981103346dc2b702891027d43f23223950e5b6bd
Kybre <hermanchan10@gmail.com>
2023-11-24 20:50:31 -0800

    [FIX] GameManager should now be able to call TransitionOut

--- Assets/Scripts/SceneTransitionBehaviour.cs
+++ Assets/Scripts/SceneTransitionBehaviour.cs
@@ -6,17 +6,15 @@ using UnityEngine.SceneManagement;
 public class SceneTransitionBehaviour : MonoBehaviour
 {
     public GameObject transitionSphere;
-    public BoxCollider carCollider;
+    Material dissolveMaterial;
     public LeanTweenType sphereFadeEase = LeanTweenType.easeInOutSine;
-    public Material dissolveMaterial;
 
-    public float minCutoff, maxCutoff, animDuration;
+    public float minCutoff, maxCutoff, enterDuration, exitDuration;
     bool sphereActive = true, transitioning = false;
 
     // Start is called before the first frame update
     void Awake()
     {
-        if (!carCollider) carCollider = GetComponent<BoxCollider>();
         if (transitionSphere && !dissolveMaterial) dissolveMaterial = transitionSphere.GetComponent<Renderer>().material;
         TransitionOut();
     }
@@ -35,13 +33,13 @@ public class SceneTransitionBehaviour : MonoBehaviour
     public void TransitionIn()
     {
         transitioning = true;
-        LeanTween.value(dissolveMaterial.GetFloat("_CutoffHeight"), maxCutoff, animDuration).setOnUpdate(UpdateMaterial).setOnComplete(ChangeScene);
+        LeanTween.value(dissolveMaterial.GetFloat("_CutoffHeight"), maxCutoff, exitDuration).setOnUpdate(UpdateMaterial).setOnComplete(ChangeScene);
     }
 
     public void TransitionOut()
     {
         transitioning = true;
-        LeanTween.value(dissolveMaterial.GetFloat("_CutoffHeight"), minCutoff, animDuration).setOnUpdate(UpdateMaterial).setOnComplete(TransitionComplete);
+        LeanTween.value(dissolveMaterial.GetFloat("_CutoffHeight"), minCutoff, enterDuration).setOnUpdate(UpdateMaterial).setOnComplete(TransitionComplete);
     }
 
     void UpdateMaterial(float cutoff)
