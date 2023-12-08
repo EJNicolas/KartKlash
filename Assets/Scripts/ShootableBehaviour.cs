988a1d6d774119c6176ca77e01da9047fba1282f
Kybre <hermanchan10@gmail.com>
2023-12-08 07:46:02 -0800

    added fade in/out, disable and visual feedback for shootables

--- Assets/Scripts/ShootableBehaviour.cs
+++ Assets/Scripts/ShootableBehaviour.cs
@@ -6,9 +6,21 @@ public class ShootableBehaviour : DamageBehaviour
 {
     CarController cc;
 
+    [Header("Dissolve")]
+    public float minCutoff;
+    public float maxCutoff;
+
+    public float disableDuration;
+    public float fadeDuration;
+
+    Material dissolveMaterial;
+    Collider col;
+
     public override void Start()
     {
         base.Start();
+        dissolveMaterial = this.GetComponent<Renderer>().material;
+        col = GetComponent<Collider>();
     }
     public override void FixedUpdate()
     {
@@ -21,5 +33,24 @@ public class ShootableBehaviour : DamageBehaviour
         player.GetComponentInChildren<Player>().health += 5f;
         cc = player.GetComponentInChildren<CarController>();
         cc.ShootableShot();
+
+        outline.OutlineWidth = 0f;
+        col.enabled = false;
+
+        LeanTween.value(dissolveMaterial.GetFloat("_CutoffHeight"), minCutoff, fadeDuration).setOnUpdate(UpdateMaterial);
+        StartCoroutine(RespawnAfterDelay(fadeDuration + disableDuration));
+    }
+
+    void UpdateMaterial(float cutoff)
+    {
+        dissolveMaterial.SetFloat("_CutoffHeight", cutoff);
+    }
+
+    IEnumerator RespawnAfterDelay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        LeanTween.value(dissolveMaterial.GetFloat("_CutoffHeight"), maxCutoff, fadeDuration)
+            .setOnUpdate(UpdateMaterial)
+            .setOnComplete(() => { col.enabled = true; });
     }
 }
---
+++ Assets/Scripts/TargetParticleBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetParticleBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public float yOffset;
+    public float speed = 5f;
+    public float delayTime = 0.25f;
+
+    private ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player) StartCoroutine(MoveParticlesTowardsPlayer(delayTime));
+    }
+
+    IEnumerator MoveParticlesTowardsPlayer(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+        int particleCount = particleSystem.GetParticles(particles);
+
+        Vector3 playerPosition = player.position;
+
+        for (int i = 0; i < particleCount; i++)
+        {
+            Vector3 directionToPlayer = playerPosition - particles[i].position;
+            directionToPlayer = new Vector3(directionToPlayer.x, directionToPlayer.y + yOffset, directionToPlayer.z); 
+                //+ player.transform.forward * player.GetComponent<Rigidbody>().velocity.magnitude;
+            particles[i].velocity += directionToPlayer.normalized * speed;
+        }
+
+        particleSystem.SetParticles(particles, particleCount);
+    }
+}
